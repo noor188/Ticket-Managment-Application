f5b05ad966b1a0ae99262bee63f5f7e7d45a3b1f
noor188 <noormmlk@gmail.com>
2025-03-05 16:19:04 -0700

    Project setup

---
+++ src/main/java/com/ticket/Ticket_Managment_System/TicketManagmentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.ticket.Ticket_Managment_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketManagmentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketManagmentSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ticket/Ticket_Managment_System/TicketManagmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticket.Ticket_Managment_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketManagmentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
