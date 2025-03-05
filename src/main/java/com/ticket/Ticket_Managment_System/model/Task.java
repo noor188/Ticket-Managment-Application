fd1bf9775262604189876c7df9cba97ab9979a78
noor188 <noormmlk@gmail.com>
2025-03-05 16:52:37 -0700

    Finished Tiekcet 2

---
+++ src/main/java/com/ticket/Ticket_Managment_System/model/Task.java
@@ -0,0 +1,20 @@
+package com.ticket.Ticket_Managment_System.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+    private String status;
+
+    @ManyToOne
+    @JoinColumn(name= "employee_id")
+    private Employee employee;
+}
